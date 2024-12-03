ce579787c7881ee0144cc8cbdc14a053e8c87245
Patrick McCarthy <p.t.mccarthy@gmail.com>
2024-12-02 23:51:53 -0800

    day 1 part 2

--- src/d01/Main.kt
+++ src/d01/Main.kt
@@ -1,6 +1,5 @@
 package d01
 
-import java.io.File
 import kotlin.math.abs
 
 fun main() {
@@ -8,11 +7,25 @@ fun main() {
     val reader = Reader()
 
     val lines = reader.readSorted("src/d01/input.txt")
+    val left = lines[0]
+    val right = lines[1]
 
+    // part one
     var sum = 0
-    for (i in 0 until lines[0].size) {
-        sum += abs(lines[0][i] - lines[1][i])
+    for (i in 0 until left.size) {
+        sum += abs(left[i] - right[i])
     }
 
     println("Sum of distances: $sum")
+
+    // part two
+    val counts: Map<Int, Int> = right.fold(mutableMapOf()) { acc, i ->
+        acc.put(i, acc.getOrDefault(i, 0) + 1)
+        acc }
+
+    val similarityScore = left.fold(0) { acc, i ->
+        acc + i.times(counts.getOrDefault(i, 0))
+    }
+
+    println("Similarity score: $similarityScore")
 }
